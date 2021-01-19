4d6459359f77237815969654ed4d66139cf0d92d
ammarkandel <merom8703@gmail.com>
2021-01-19 23:31:31 +0200

    add more tests

--- spec/check_game_result_spec.rb
+++ spec/check_game_result_spec.rb
@@ -1,19 +1,38 @@
-require './lib/check_game_result'
+require '../lib/check_game_result'
 
 describe CheckGameResult do
   describe '#win' do
     board = ['', '', '', '', '', '', '', '', '']
     let(:check_game_result) { CheckGameResult.new(board) }
-    it 'check the result if win case happen' do
+    let(:expect_false) { expect(check_game_result.win).to eql(false) } 
+    it 'check the result of the win method. If it win case' do
       board = ['X', 'X', 'X', '', '', '', '', '', '']
       check_game_result
       expect(check_game_result.win).to eql(true)
     end
 
-    it 'check the result if win case not happen' do
+    it 'check the result of the win method. If not win case' do
       board = ['X', 'O', 'X', '', '', '', '', '', '']
       check_game_result
-      expect(check_game_result.win).to eql(false)
+      expect_false
+    end
+
+    it 'check the result of the win method. If nothing in the board' do
+      board = ['', '', '', '', '', '', '', '', '']
+      check_game_result
+      expect_false
+    end
+
+    it 'check the result of the win method. If playing number less than 3' do
+      board = ['X', 'O', '', '', '', '', '', '', '']
+      check_game_result
+      expect_false
+    end
+
+    it 'check the result of the win method. If no player win' do
+      board = ['X', 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O']
+      check_game_result
+      expect_false
     end
   end
 end
